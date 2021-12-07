0478c5345718a9d40b47ed15c598389896bdaf8a
JAKeijzer96 <jasperkeijzer96@gmail.com>
2021-12-07 20:40:39 +0100

    restructure to group by year

---
+++ 2021/day8.py
@@ -0,0 +1,11 @@
+def read_file():
+    lines = []
+    with open("./resources/day8input.txt") as file:
+        lines = file.readlines()
+
+def main():
+    
+    pass
+
+if __name__ == '__main__':
+    main()
